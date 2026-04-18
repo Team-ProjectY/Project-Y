b037f3364236e81d3684bb5d46f4345ac2d13b2b
jo-hyun-sung1469 <hunsung1469@gmail.com>
2026-04-19 01:52:57 +0900

    feat: 바닥 감지 기능 추가/플레이어 회전 방지
    #1

--- Project-Y/Assets/Script/Player/PlayerMovement.cs
+++ Project-Y/Assets/Script/Player/PlayerMovement.cs
@@ -1,17 +1,30 @@
 using UnityEngine;
 
+[RequireComponent(typeof(Rigidbody))]
 public class PlayerMovement : MonoBehaviour
 {
     private Rigidbody _rigidbody;
     private float _jumpForce = 10f;
+    private bool _isGrounded;
+    private float _rayDistance;
+    private CapsuleCollider _collider;
+    //이 콜라이더는 임시용입니다 플레이어에 들어갈 콜라이더로 변경해야됩니다
     void Awake()
     {
         TryGetComponent<Rigidbody>(out _rigidbody);
+        _rigidbody.freezeRotation = true;
+        _rayDistance = _collider != null ? (_collider.height / 2f) + 0.1f : 1.1f;
+    }
+
+    void FixedUpdate()
+    {
+        _isGrounded = Physics.Raycast(transform.position, Vector3.down, _rayDistance, LayerMask.GetMask("Ground"));
     }
 
     public void Jump()
     {
-        _rigidbody.AddForce(Vector3.up * _jumpForce, ForceMode.Impulse);
+        if(_isGrounded)
+            _rigidbody.AddForce(Vector3.up * _jumpForce, ForceMode.Impulse);
     }
 
     public void Move(Vector3 movedir)
