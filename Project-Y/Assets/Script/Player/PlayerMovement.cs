d84376cc209c29594786450c03ce0f329eb1c4a8
jo-hyun-sung1469 <hunsung1469@gmail.com>
2026-04-20 01:04:22 +0900

    fix: 레이어 마스크 값을 캐싱하도록 개선
    #6

--- Project-Y/Assets/Script/Player/PlayerMovement.cs
+++ Project-Y/Assets/Script/Player/PlayerMovement.cs
@@ -3,24 +3,28 @@
 [RequireComponent(typeof(Rigidbody))]
 public class PlayerMovement : MonoBehaviour
 {
-    private Rigidbody _rigidbody;
     private float _jumpForce = 10f;
     private float _normalspeed = 10f;
     private bool _isGrounded;
     private float _rayDistance;
-    private CapsuleCollider _collider;
+    private LayerMask _layerMask;
+
+    private Rigidbody _rigidbody;
+    [SerializeField] private CapsuleCollider _collider;
     //이 콜라이더는 임시용입니다 플레이어에 들어갈 콜라이더로 변경해야됩니다.
+
     void Awake()
     {
         TryGetComponent<Rigidbody>(out _rigidbody); 
         TryGetComponent<CapsuleCollider>(out _collider);
         _rigidbody.freezeRotation = true;
         _rayDistance = _collider != null ? (_collider.height / 2f) + 0.1f : 1.1f;
+        _layerMask = LayerMask.GetMask("Ground");
     }
 
     void FixedUpdate()
     {
-        _isGrounded = Physics.Raycast(transform.position, Vector3.down, _rayDistance, LayerMask.GetMask("Ground"));
+        _isGrounded = Physics.Raycast(transform.position, Vector3.down, _rayDistance, _layerMask);
     }
 
     public void Jump()
