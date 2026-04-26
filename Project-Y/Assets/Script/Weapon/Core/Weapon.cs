ec5c29cf0eafc2acc7dc740208c590d92ad9281d
nelu572 <s25031@gsm.hs.kr>
2026-04-27 00:31:16 +0900

    feat: TestGun 추가, 조준 구현
    #7

--- Project-Y/Assets/Script/Weapon/Core/Weapon.cs
+++ Project-Y/Assets/Script/Weapon/Core/Weapon.cs
@@ -4,79 +4,25 @@ public class Weapon : MonoBehaviour
 {
     [SerializeField] private WeaponSOData _data;
 
-    private float _lastFireTime;
-    private int _currentAmmo;
-    private bool _isReloading;
     private bool _isFiring;
 
     public WeaponSOData Data => _data;
 
-    void Awake()
-    {
-        _currentAmmo = Data.MagazineSize;
-    }
-
-    void Update()
-    {
-        if (_isFiring)
-            TryFire();
-    }
-
     public void StartFire()
     {
-        if (_isReloading) return;
-
-        if (Data.IsAutomatic)
-            _isFiring = true;
-        else
-            TryFire();
+        /// TODO: 발사 조건 검사, 발사 구현
+        /// 조건: 장전중, 단발/연사 ,발사 속도
     }
 
     public void StopFire()
     {
+        // 연사 총 멈추기
         _isFiring = false;
-    }
-
-    void TryFire()
-    {
-        if (Time.time < _lastFireTime + (1f / Data.FireRate))
-            return;
-
-        if (_currentAmmo <= 0)
-        {
-            Reload();
-            return;
-        }
-
-        Fire();
-    }
-
-    void Fire()
-    {
-        _lastFireTime = Time.time;
-        _currentAmmo--;
-
-        Ray ray = Camera.main.ViewportPointToRay(new Vector3(0.5f, 0.5f));
-        if (Physics.Raycast(ray, out RaycastHit hit, Data.Range))
-        {
-            // TODO: 데미지 처리
-        }
-
-        // TODO: 반동
     }
 
     public void Reload()
     {
-        if (_isReloading) return;
-
-        _isReloading = true;
-        Invoke(nameof(FinishReload), Data.ReloadTime);
-    }
-
-    void FinishReload()
-    {
-        _currentAmmo = Data.MagazineSize;
-        _isReloading = false;
+        // TODO: 재장전
     }
 
     public void OnEquip()
